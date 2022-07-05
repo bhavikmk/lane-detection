b5256bfed218f2138baf578400b4b2fe37ed4685
bhavikmk <u19me151@med.svnit.ac.in>
2022-07-06 01:03:24 +0530

    Arrange video

--- src/main.py
+++ src/main.py
@@ -2,17 +2,13 @@ import imp
 import cv2 
 import numpy as np
 
-video = cv2.VideoCapture('/home/bhavik/projects/compVision/lane_detection/Highway - 10364.mp4')
+video = cv2.VideoCapture('/home/bhavik/projects/compVision/lane_detection/test_video/Highway - 10364.mp4')
 
 while(video.isOpened()):
     ret, img = video.read()
     if ret == True:
         
         # Fill lane detection algotithm
-
-
-        # kernel = np.ones((5,5),np.float32)/25
-        # dst = cv2.filter2D(img,-1,kernel)
 
         blur = cv2.GaussianBlur(img,(5,5),0)
         edges = cv2.Canny(blur,100,200)
@@ -32,19 +28,16 @@ while(video.isOpened()):
         min_line_length = 10
         max_line_gap = 1
 
-        line_image = np.copy(masked_edges)*0 #creating a blank to draw lines on
+        line_image = np.copy(masked_edges)*0 
 
-        # Run Hough on edge detected image
         lines = cv2.HoughLinesP(masked_edges, rho, theta, threshold, np.array([]),
-                                    min_line_length, max_line_gap)
+                                min_line_length, max_line_gap)
 
-        # Iterate over the output "lines" and draw lines on the blank
         for line in lines:
             for x1,y1,x2,y2 in line:
-                cv2.line(masked_edges,(x1,y1),(x2,y2),(255,0,0),5)
+                cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
 
-        # Draw the lines on the edge image
-        combo = cv2.addWeighted(masked_edges, 0.8, masked_edges, 1, 0) 
+        combo = cv2.addWeighted(masked_edges, 0.2, line_image, 1, 0) 
         cv2.imshow("Frame", combo)
 
         if cv2.waitKey(25) & 0xFF == ord('q'):
